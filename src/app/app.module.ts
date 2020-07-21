7bc0b726ba5e5b7edc08ef56a80ee6750f724f1d
ajml0012 <ajml0012@red.ujaen.es>
2020-07-21 11:45:07 +0200

    Creación componentes INICIO,NAV,FOOTER

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
