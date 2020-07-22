52acf9c165434a64a11e32e71f36c42c7a7dde43
Francisco Jesús Najas Martos <fjnm0006@red.ujaen.es>
2020-07-22 11:20:39 +0200

    Terminado formulario creacion de eventos

--- src/app/componentes/creacion-evento/creacion-evento.component.ts
+++ src/app/componentes/creacion-evento/creacion-evento.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Evento } from 'src/app/clases/evento';
+import { TipoEvento } from '../../enum/tipo-evento.enum';
+import { CategoriaEvento } from 'src/app/enum/categoria-evento.enum';
 
 
 @Component({
   selector: 'app-creacion-evento',
   templateUrl: './creacion-evento.component.html',
-  styleUrls: ['./creacion-evento.component.css']
+  styleUrls: ['./creacion-evento.component.css'],
+
 })
+
 export class CreacionEventoComponent implements OnInit {
-  constructor(public evento: Evento) { }
+  
+  tipoEvento = TipoEvento;
+  categoriaEvento = CategoriaEvento;
+
+  constructor() { 
+  }
 
   ngOnInit() {
     
   }
-
-
 
 }
