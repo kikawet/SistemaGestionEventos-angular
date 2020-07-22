ef10a0b73fb9e3336a917ed55d51a582054e997b
ajml0012 <ajml0012@red.ujaen.es>
2020-07-22 12:37:04 +0200

    configuración y solución de CORS peticion lista de evento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,22 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { NavComponent } from './componentes/nav/nav.component';
 import { FooterComponent } from './componentes/footer/footer.component';
-
+import { ServicioEventoService } from './servicios/servicio-evento.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     NavComponent,
     FooterComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ServicioEventoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
