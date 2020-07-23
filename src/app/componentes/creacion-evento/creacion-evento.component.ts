9d67ecf7211c1e800b66f7c791ece9bb3400d10a
ajml0012 <ajml0012@red.ujaen.es>
2020-07-23 13:00:53 +0200

    Componente y conexión con backend en creación de evento terminada

--- src/app/componentes/creacion-evento/creacion-evento.component.ts
+++ src/app/componentes/creacion-evento/creacion-evento.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Evento } from 'src/app/clases/evento';
 import { TipoEvento } from '../../enum/tipo-evento.enum';
 import { CategoriaEvento } from 'src/app/enum/categoria-evento.enum';
+import { Url } from 'url';
+import { ServicioEventoService } from 'src/app/servicios/servicio-evento.service';
+import { Router } from '@angular/router';
+import { ServicioUsuarioService } from 'src/app/servicios/servicio-usuario.service';
 
 
 @Component({
@@ -12,15 +16,55 @@ import { CategoriaEvento } from 'src/app/enum/categoria-evento.enum';
 })
 
 export class CreacionEventoComponent implements OnInit {
-  
+
+  public titulo: string;
+  public descripcion: string;
+  public tipoE: TipoEvento;
+  public categoria: CategoriaEvento;
+  public lugar: string;
+  public fecha: Date;
+  public aforo: number;
+  public foto: string;
+
   tipoEvento = TipoEvento;
   categoriaEvento = CategoriaEvento;
 
-  constructor() { 
+  constructor(private eventoServicio: ServicioEventoService, private router: Router,private servicioUsuario: ServicioUsuarioService) {
   }
 
   ngOnInit() {
+    if(this.servicioUsuario.idUsuario==null){
+      this.router.navigate(["login"]);
+    }
+  }
+
+  crearEvento() {
+    if(this.servicioUsuario.idUsuario==null){
+      this.router.navigate(["login"]);
+    }
+    console.log(this.titulo + "-" + this.descripcion + "-" + this.tipoE.valueOf() + "-" + this.categoria.valueOf() + "-" + this.lugar + "-" + this.fecha + "-" + this.aforo + "-" + this.foto);
+
+    let evento: Evento=new Evento();
+    evento.titulo = this.titulo;
+    evento.descripcion = this.descripcion;
+    evento.tipo = this.tipoE;
+    evento.categoriaEvento = this.categoria;
+    evento.lugar = this.lugar;
+    evento.fecha = this.fecha;
+    evento.aforoMaximo = this.aforo;
+    evento.foto = this.foto;
+
+    this.eventoServicio.crearEvento(evento).subscribe(data => {
+      console.log(data);
+      this.router.navigate(["eventos"]);
+    },
+      err => {
+        console.error('Error en la creación de evento');
+        console.error(err);
+      });
+
     
   }
+
 
 }
--- src/app/servicios/servicio-evento.service.ts
+++ src/app/servicios/servicio-evento.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpClientModule } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpClientModule, HttpParams } from "@angular/common/http";
 import { Observable } from 'rxjs';
+import { ServicioUsuarioService } from './servicio-usuario.service';
+import { Evento } from '../clases/evento';
+import { TipoEvento } from '../enum/tipo-evento.enum';
+import { CategoriaEvento } from '../enum/categoria-evento.enum';
+import { Url } from 'url';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 
-    'Authorization':'Basic '+ btoa("Patricio Ruiz:1234")    
-  })
+  headers: new HttpHeaders({
+    'Authorization': 'Basic ' + btoa("Patricio Ruiz:1234")
+  }),
+  params: new HttpParams()
 };
 
 @Injectable({
@@ -13,13 +19,33 @@ const httpOptions = {
 })
 export class ServicioEventoService {
 
-  constructor(private http: HttpClient) { }
-
-  getListaEventos():Observable<any>{
-    return this.http.get("http://localhost:12021/sge-api/rest/evento", httpOptions);
+  constructor(private http: HttpClient, private servicioUsuario: ServicioUsuarioService) {
+    this.idUsuario = this.servicioUsuario.idUsuario;
+  }
+  public idUsuario: string;
+  private rootPath = "http://localhost:12021/sge-api/rest/evento";
+  getListaEventos(): Observable<any> {
+    return this.http.get(this.rootPath, httpOptions);
   }
 
-  
+  crearEvento(evento:Evento):Observable<any> {
+    this.idUsuario=this.servicioUsuario.idUsuario;
+    const myParams = new HttpParams()
+      .set("id", this.idUsuario);
+    httpOptions.params=myParams;
+
+    //return this.http.get(this.rootPath + "/" + this.idUsuario, options);
+
+    
+
+    console.log("creacion evento petición");
+
+    return this.http.post(this.rootPath, evento, httpOptions);
+
+    
+  }
+
+
 
 
 }
